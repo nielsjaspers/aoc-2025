b8e914f055d28b1a215c2f08e61ee8c9fbda88d7
Niels Jaspers <gh@nielsjaspers.com>
2025-12-01 16:32:06 +0100

    day 1, part 2. try 2
    solution; ~480-450 µs

--- day1_php/src/main.php
+++ day1_php/src/main.php
@@ -1,71 +1,34 @@
 <?php
 
 $total = 50;
-
 $count = 0;
-
 $time_start = microtime(true);
 
 $lines = file('data/day1/day1.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 foreach ($lines as $line) {
-    $result = sscanf($line, "%c%d");
-    calculate($result[0], $result[1]);
+    $ticks = (int)substr($line,1);
+    $f = intdiv($ticks,100);
+    $count += $f;
+
+    $ticks = $ticks % 100;
+
+    if ($line[0] === 'R') {
+        $new = $total + $ticks;
+        $count += intdiv($new, 100);  // 1 if crossed, 0 if not
+        $total = $new % 100;
+    } else {
+        $new = $total - $ticks;
+        if ($total > 0 && $new <= 0) $count++;
+        $total = ($new % 100 + 100) % 100;
+    }
 }
 
-// calculate("L",50);
 echo "Count: $count\n";
 
 // time in microseconds
 $time = (microtime(true) - $time_start) * 1_000_000;
-
 echo "Time: $time µs\n";
 
-
-function calculate($direction, $ticks) {
-    global $total;
-    global $count;
-
-    $f = intdiv($ticks,100);
-    $ticks = $ticks % 100;
-
-    if ($f > 0){
-        $count += $f;
-    }
-    // echo "Direction: $direction, Ticks: $ticks, Total: $total\n";
-    if ($direction === 'R') {
-        $total = upperBound($total, $ticks);
-        // echo "Upper bound: $total\n";
-    } else {
-        $total = lowerBound($total, $ticks);
-        // echo "Lower bound: $total\n";
-    }
-}
 echo "Count: $count\n";
-
-function upperBound($tot, $ticks) {
-    global $count;
-    if ($tot + $ticks > 99) {
-        $count++;
-        // echo "Passed 100!!\n";
-        return $tot + $ticks - 100;
-    }
-    return $tot + $ticks;
-}
-
-function lowerBound($tot, $ticks) {
-    global $count;
-    $result = $tot - $ticks;
-    
-    if ($tot > 0 && $result <= 0) {
-        $count++;
-        // echo "Passed 0!!\n";
-    }
-    
-    if ($result < 0) {
-        return $result + 100;
-    }
-    return $result;
-}
-
 ?>
