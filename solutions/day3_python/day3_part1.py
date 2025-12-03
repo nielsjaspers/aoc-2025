55d9b024628479188e34b5e8afd1232be8fd78b2
Niels Jaspers <gh@nielsjaspers.com>
2025-12-03 22:45:27 +0100

    day 3, part 1. try 4
    solution; ~1.45 ms

--- solutions/day3_python/day3_part1.py
+++ solutions/day3_python/day3_part1.py
@@ -3,37 +3,36 @@ import time
 start_time = time.time()
 
 def sortDigits(line: str) -> int:
-    num1 = int(line[0])
-    num2 = 0
+    num1 = line[0]
+    num2 = '0'
     n = len(line)
 
     for i in range(1, n - 1):
-        digit = int(line[i])
-        if num1 == 9 and num2 == 9:
+        digit = line[i]
+        if num1 == '9' and num2 == '9':
             return 99
         if num1 < digit:
             num1 = digit
-            num2 = 0
+            num2 = '0'
         elif num2 < digit and num1 != digit:
             num2 = digit
         elif num1 == digit:
             num2 = digit
 
-    digit = int(line[n - 1])
-    if num1 != 9 or num2 != 9:
+    digit = line[n - 1]
+    if num1 != '9' or num2 != '9':
         if num2 < digit:
             num2 = digit
 
-    return num1 * 10 + num2
+    # convert characters to integers by subtracting 48 (ASCII value of '0')
+    return (ord(num1) - 48) * 10 + (ord(num2) - 48)
     
 total = 0
 
 with open('data/day3/day3.txt', 'r') as f:
-    for line in f:
-        line = line.rstrip('\n')
-
+    lines = f.read().splitlines()
+    for line in lines:
         total += sortDigits(line)
-        # print(sorted_digits)
 
 end_time = time.time()
 print("Time taken: ", (end_time - start_time) * 1000, "ms")
