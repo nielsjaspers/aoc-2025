5454b7fd182497485baf737e5e644dba227fceee
Niels Jaspers <gh@nielsjaspers.com>
2025-12-03 22:26:50 +0100

    day 3, part 1. try 2
    solution; ~2.2 ms

--- solutions/day3_python/day3_part1.py
+++ solutions/day3_python/day3_part1.py
@@ -2,12 +2,16 @@ import time
 
 start_time = time.time()
 
-def sortDigits(digits: list[int]) -> int:
-    num1 = 0
+def sortDigits(line: str) -> int:
+    num1 = int(line[0])
     num2 = 0
+    n = len(line)
 
-    for i, digit in enumerate[int](digits):
-        if i != len(digits) - 1:
+    for i in range(1, n):
+        digit = int(line[i])
+        if num1 == 9 and num2 == 9:
+            return 99
+        if i != n - 1:
             # print(digit,num1,num2)
             if num1 < digit:
                 num1 = digit
@@ -23,21 +27,17 @@ def sortDigits(digits: list[int]) -> int:
             if num2 < digit:
                 num2 = digit
                 continue
-            if num2 == digit:
-                num2 = digit
-                continue
 
     # returning num1 * 10 + num2 is faster than first converting to string and then converting to int to append the digits
     return (num1 * 10) + num2
+
 total = 0
 
 with open('data/day3/day3.txt', 'r') as f:
-    for line_number, line in enumerate(f):
-        line = line.strip()
+    for line in f:
+        line = line.rstrip('\n')
 
-        digits = [int(char) for char in line]
-        sorted_digits = sortDigits(digits)
-        total += sorted_digits
+        total += sortDigits(line)
         # print(sorted_digits)
 
 end_time = time.time()
