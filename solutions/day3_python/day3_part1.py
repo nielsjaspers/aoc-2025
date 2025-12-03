3418034ab8c6d862339daa6cde1d5cc2069ecc1e
Niels Jaspers <gh@nielsjaspers.com>
2025-12-03 22:59:31 +0100

    day 3, part 1. try 5
    solution; ~1.22 ms
    
    apparently updating python to 3.14 makes it run even faster (went from
    1.35 ms to 1.22 ms)
    
    thanks python. very cool.

--- solutions/day3_python/day3_part1.py
+++ solutions/day3_python/day3_part1.py
@@ -1,25 +1,21 @@
-import time
+import datetime
 
-start_time = time.time()
+start_time = datetime.datetime.now()
 
 def sortDigits(line: str) -> int:
     num1 = line[0]
     num2 = '0'
-    n = len(line)
 
-    for i in range(1, n - 1):
-        digit = line[i]
+    for digit in line[1:-1]:
         if num1 == '9' and num2 == '9':
             return 99
         if num1 < digit:
             num1 = digit
             num2 = '0'
-        elif num2 < digit and num1 != digit:
-            num2 = digit
-        elif num1 == digit:
+        elif num1 == digit or num2 < digit:
             num2 = digit
 
-    digit = line[n - 1]
+    digit = line[-1]
     if num1 != '9' or num2 != '9':
         if num2 < digit:
             num2 = digit
@@ -34,6 +30,7 @@ with open('data/day3/day3.txt', 'r') as f:
     for line in lines:
         total += sortDigits(line)
 
-end_time = time.time()
-print("Time taken: ", (end_time - start_time) * 1000, "ms")
+end_time = datetime.datetime.now()
+time_taken = (end_time - start_time).total_seconds() * 1000
+print("Time taken: ", time_taken, "ms")
 print("Total: ", total)
